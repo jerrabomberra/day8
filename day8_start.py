ac577c2186f2d399ef0beb946f939ca3ad4ab31f
Douglas Gordon <gordononline@bigpond.com>
2023-09-21 19:43:41 +1000

    hello

--- day8_start.py
+++ day8_start.py
@@ -2,3 +2,10 @@
 # Create a function called greet(). 
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
+
+def greet(name):
+    print("good day")
+    print("Thursday")
+    return(f"Hello {name}").title()
+
+print(greet("doug"))
